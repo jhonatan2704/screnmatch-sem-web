c1286ad5267176498cb2c77bf8dc7f2d9f0eed1e
Jhonatan Vicente <vjhonatan356@gmail.com>
2026-01-06 14:49:13 -0300

    criando os dados serie

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,14 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSeries;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,11 +19,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Digite a série que deseja fazer a busca:");
+		var nomeSerie = leitura.nextLine();
+		var enderenco = "https://www.omdbapi.com/?t=" + nomeSerie + "&apikey=6585022c";
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados(enderenco);
+
 		ConverteDados conversor = new ConverteDados();
 		DadosSeries dados = conversor.obterdados(json, DadosSeries.class);
 		System.out.println(dados);
+
+		System.out.println("Temporada: ");
+		var temporada = leitura.nextLine();
+		System.out.println("Episodio: ");
+		var episodio = leitura.nextLine();
+		enderenco = "https://www.omdbapi.com/?t=" + nomeSerie + "&" + temporada + "&" + episodio + "&apikey=6585022c";
+		json = consumoApi.obterDados(enderenco);
+		DadosEpisodio dadosEpisodio = conversor.obterdados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
